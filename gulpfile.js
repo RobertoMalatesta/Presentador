1d6e3e660b92d44bb8b422a511e55806b374073c
Abhinav Madahar <abhinavmadahar@gmail.com>
2015-06-22 21:59:58 -0400

    Minifies self-hosted CSS and Stylus files

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var coffee = require('gulp-coffee');
 var uglify = require('gulp-uglify');
 var browserify = require('gulp-browserify');
 var stylus = require('gulp-stylus');
+var minify = require('gulp-minify-css');
 var logarithmic = require('logarithmic');
 
 gulp.task("default", ["scripts", "styles", "images", "watch"], function() {
@@ -19,12 +20,14 @@ gulp.task("scripts", function () {
 
 gulp.task("css", function() {
 	gulp.src("source/styles/*.css")
+		.pipe(minify())
 		.pipe(gulp.dest("public/styles"))
 });
 
 gulp.task("stylus", function() {
 	gulp.src("source/styles/*.styl")
 		.pipe(stylus())
+		.pipe(minify())
 		.pipe(gulp.dest("public/styles"))
 });
 
