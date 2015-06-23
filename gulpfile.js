b92b6a6b1f72342668648a402c9fb15b26fd1b41
Abhinav Madahar <abhinavmadahar@gmail.com>
2015-06-22 20:21:57 -0400

    Uglifies Javascript

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var coffee = require('gulp-coffee');
+var uglify = require('gulp-uglify');
 var logarithmic = require('logarithmic');
 
 gulp.task("default", ["scripts", "styles", "images", "watch"], function() {
@@ -9,6 +10,7 @@ gulp.task("default", ["scripts", "styles", "images", "watch"], function() {
 gulp.task("scripts", function () {
 	gulp.src("source/scripts/*.coffee")
 		.pipe(coffee({bare: true}).on('error', logarithmic.error))
+		.pipe(uglify())
 		.pipe(gulp.dest("public/scripts"))
 });
 
